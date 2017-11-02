9fbbb29bc736e6716a10138bc38881a6a765baf2
if1live <libsora25@gmail.com>
2017-11-02 11:39:00 +0900

    initial commit

---
+++ Assets/Mihiro/Scripts/Demo/ClickableButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Mihiro.Scripts.Demo {
+    public class ClickableButton : MonoBehaviour {
+        [SerializeField]
+        Button button = null;
+
+        [SerializeField]
+        Text text = null;
+
+        int clickCount = 0;
+        void Start() {
+            button.onClick.AddListener(OnClick);
+            text.text = string.Format("click: {0}", clickCount);
+        }
+
+        void OnClick() {
+            clickCount += 1;
+            text.text = string.Format("click: {0}", clickCount);
+        }
+    }
+}
